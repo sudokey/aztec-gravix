9038e2dfa26be40550ea42823f20b7624ba602c9
Maga <steam.30mb1@gmail.com>
2023-10-16 05:15:31 +0400

    Up

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
-import { BlankContractArtifact } from './artifacts/Blank.js';
+import { VaultContractArtifact } from './artifacts/Vault.ts';
 import { PXE, createPXEClient } from '@aztec/aztec.js';
 import { ContractArtifact } from '@aztec/foundation/abi';
 
 // update this if using a different contract
 
-export const contractArtifact: ContractArtifact = BlankContractArtifact;
+export const contractArtifact: ContractArtifact = VaultContractArtifact;
 
 export const PXE_URL: string = process.env.PXE_URL || 'http://localhost:8080';
 export const pxe: PXE = createPXEClient(PXE_URL);
